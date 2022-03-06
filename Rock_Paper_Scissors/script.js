797305155f7c7dc59d74e9a7fbe4ed890a2e91c0
Nicolette Staley <emmyandrhys@gmail.com>
2022-03-06 15:45:37 -0500

    rps ui works!

--- Rock_Paper_Scissors/script.js
+++ Rock_Paper_Scissors/script.js
@@ -37,14 +37,14 @@ function game() {
   else {
     winnerDialog.textContent ='You lose.  The computer "rocked" you.  It "cut" you up.'
   }
+  winnerDialog.style.visibility='visible';
+  selectiontext.style.visibility='hidden';
 }
 var wait = true;
 
-var selectiontext = "What is your choice?  Rock, Paper, or Scissors?";
-
 const selectionsBox = document.getElementById("selections");
 
-const playerWait = document.getElementById('player_wait');
+/* const playerWait = document.getElementById('player_wait');
 const playerRock = document.getElementById('player_rock');
 const playerPaper = document.getElementById('player_paper');
 const playerScissors = document.getElementById('player_scissors');
@@ -53,57 +53,60 @@ const computerWait = document.getElementById('computer_wait');
 const computerRock = document.getElementById('computer_rock');
 const computerPaper = document.getElementById('computer_paper');
 const computerScissors = document.getElementById('computer_scissors');
+ */
 
+const playerSelected = document.getElementById('player_selection');
+const computerSelected = document.getElementById('computer_selection');
+const selectiontext = document.getElementById('selection_text');
 var computerScore = 0;
 var playerScore = 0;
 var rounds = 0;
 
 const playerScoreDisplay = document.getElementById('player_score');
-const gamesPlayed = document.getElementById('games_played');
+const gamesPlayed = document.getElementById('matches_played');
 const computerScoreDisplay = document.getElementById('computer_score');
 
 const RPSwinLose = ['You win! Rock smashes Scissors!', 'You lose! Paper covers Rock!', 'You win! Scissors cut Paper!', 'You lose! Rock smashes Scissors!', 'You win! Paper covers Rock!', 'You lose! Scissors cut Paper!']
 
 function playRound(playerSelection) {
   var computerSelection = computerPlay();
-  playerWait.style.visibility='hidden';
-  if(playerSelection==='rock'){playerRock.style.visibility='visible'};
-  if(playerSelection==='paper'){playerPaper.style.visibility='visible'};
-  if(playerSelection==='scissors'){playerScissors.style.visibility='visible'};
-  if(computerSelection==='rock'){computerRock.style.visibility='visible'};
-  if(computerSelection==='paper'){computerPaper.style.visibility='visible'};
-  if(computerSelection==='scissors'){computerScissors.style.visibility='visible'};
+  if(playerSelection==='rock'){playerSelected.src='./rock.svg'};
+  if(playerSelection==='paper'){playerSelected.src='./paper_ball.svg'};
+  if(playerSelection==='scissors'){playerSelected.src='./scissors.svg'};
+  if(computerSelection==='rock'){computerSelected.src='./rock.svg'};
+  if(computerSelection==='paper'){computerSelected.src='./paper_ball.svg'};
+  if(computerSelection==='scissors'){computerSelected.src='./scissors.svg'};
 
   if (playerSelection === computerSelection) {
-    selectiontext = "It's a tie.  Try again.";
+    selectiontext.textContent = "It's a tie.  Try again.";
     wait = true;
     return;
   }
   if (playerSelection === 'rock') {
     if (computerSelection === 'scissors') {
       playerScore +=1;
-      selectiontext = RPSwinLose[0];
+      selectiontext.textContent = RPSwinLose[0];
         } else {
       computerScore += 1;
-      selectiontext = RPSwinLose[1];
+      selectiontext.textContent = RPSwinLose[1];
     } rounds += 1;
   }
   if (playerSelection === 'scissors') {
     if (computerSelection === 'paper') {
       playerScore +=1;
-      selectiontext = RPSwinLose[2];
+      selectiontext.textContent = RPSwinLose[2];
     } else {
   computerScore += 1;
-  selectiontext = RPSwinLose[3];
+  selectiontext.textContent = RPSwinLose[3];
 } rounds += 1;
   }
   if (playerSelection === 'paper') {
     if (computerSelection === 'rock') {
       playerScore +=1;
-      selectiontext = RPSwinLose[4];
+      selectiontext.textContent = RPSwinLose[4];
         } else {
       computerScore += 1;
-      selectiontext = RPSwinLose[5];
+      selectiontext.textContent = RPSwinLose[5];
     } rounds += 1;
   }
   console.log(`Player Score: ${playerScore}`)
