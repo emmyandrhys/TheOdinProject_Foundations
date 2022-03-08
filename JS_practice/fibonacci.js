432964230e96e857aaeac023413b71a329e99294
Nicolette Staley <emmyandrhys@gmail.com>
2022-03-08 00:08:04 -0500

    fibonacchi

--- JS_practice/fibonacci.js
+++ JS_practice/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+let fib = [1, 1];
+if (term < 0){ return "OOPS"}
+term = parseInt(term);
+if (term > 2) {
+	for (let x=1; x <term; x++){
+		let next = fib[x] + fib[x-1];
+		fib.push(next);
+	}
+}return fib[term - 1];
 };
 
 // Do not edit below this line
