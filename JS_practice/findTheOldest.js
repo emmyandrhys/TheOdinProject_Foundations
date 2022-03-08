97c8ad82ad97782b010885ef2550f4f707e0da3b
Nicolette Staley <emmyandrhys@gmail.com>
2022-03-08 00:08:14 -0500

    find oldest

--- JS_practice/findTheOldest.js
+++ JS_practice/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+let year = new Date().getFullYear();
+for (person of people) {
+	if (person.yearOfDeath) { }
+	else (person.yearOfDeath = year)
+}
+var sorted = people.sort((a,b)=> (a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ? 1: -1, 0)
+return sorted[0]
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
