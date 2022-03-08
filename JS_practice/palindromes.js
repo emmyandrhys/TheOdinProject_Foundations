3b0ef751299cbead86102fb8009c7558973494c7
Nicolette Staley <emmyandrhys@gmail.com>
2022-03-07 23:22:15 -0500

    palindromes done

--- JS_practice/palindromes.js
+++ JS_practice/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (palindromes) {
+	let clean = "";
+	let palindome = palindromes.toLowerCase();
+	let regex = /[a-z]/
+	for (char of palindome) {
+		 if (regex.test(char) ) {
+			clean += char;
+		 }
+	} for (let i=0; i <clean.length; i++){
+		let j = clean.length - i - 1;
+		if (clean[i] != clean[j]) { return false}
+	} return true;
 };
 
 // Do not edit below this line
